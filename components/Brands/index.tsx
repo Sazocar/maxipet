917dccef90ee99aef5ca41937f888215bcc6318f
Simón A Azócar <saimn.azocar@gmail.com>
2023-06-23 12:29:46 -0400

    FIX brand image spacing

--- components/Brands/index.tsx
+++ components/Brands/index.tsx
@@ -3,15 +3,31 @@ import Heading from '../Heading';
 const Brands = () => (
   <section className="mx-auto w-5/6 my-20">
     <Heading text="Trabajamos con las mejores marcas" />
-    <div className="flex flex-col mt-12 items-center md:flex-row justify-evenly">
-      <h2 className="w-full mx-6">
+    <div className="flex flex-col mt-12 gap-y-6 items-center md:flex-row justify-evenly">
+      <h2 className="w-full">
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur quam
         ipsum, ex eveniet eligendi dolorem
       </h2>
-      <img src="/brand.png" alt="brand" className="w-32 h-full object-cover" />
-      <img src="/brand.png" alt="brand" className="w-32 h-full object-cover" />
-      <img src="/brand.png" alt="brand" className="w-32 h-full object-cover" />
-      <img src="/brand.png" alt="brand" className="w-32 h-full object-cover" />
+      <img
+        src="/brand.png"
+        alt="brand"
+        className="w-32 mx-6 h-full object-cover"
+      />
+      <img
+        src="/brand.png"
+        alt="brand"
+        className="w-32 mx-6 h-full object-cover"
+      />
+      <img
+        src="/brand.png"
+        alt="brand"
+        className="w-32 mx-6 h-full object-cover"
+      />
+      <img
+        src="/brand.png"
+        alt="brand"
+        className="w-32 mx-6 h-full object-cover"
+      />
     </div>
   </section>
 );
