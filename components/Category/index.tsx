a5ae3bc4e8b94b9b8e0e734ae3eb01844b5256d4
Simón A Azócar <saimn.azocar@gmail.com>
2023-06-23 12:32:28 -0400

    FIX category image size

--- components/Category/index.tsx
+++ components/Category/index.tsx
@@ -2,7 +2,7 @@ const Category = ({ item }) => {
   const { image, text } = item;
 
   return (
-    <figure className="w-40 p-4 h-full flex flex-col items-center">
+    <figure className="w-52 p-4 h-full flex flex-col items-center">
       <img src={image} alt={image} />
       <span className="mt-4">{text}</span>
     </figure>
